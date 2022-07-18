5c7a823fb089400f63b827cad280784906926f80
Omkar R <omkaraut07@gmail.com>
2022-07-18 19:06:44 +0530

    adding dynam for lockl table

--- backend.tf
+++ backend.tf
@@ -2,5 +2,7 @@ terraform {
   backend "s3" { 
     bucket = "talent-academy-tf-by-omkar"
     key = "Talent-Academy/Terraform_Labs/backend_tf_lab/terraform.tfstate"
+    region = "eu-west-1"
+    dynamodb_table = "terraform-lock"
   }
 }
